cb45abfe12602febb7aca9ff952fe919f7982c5c
pavelp <pavel.petrov@asseco.lt>
2016-08-06 18:20:06 +0300

    dizain

--- src/app/tours/tours-search/tours-list/tours-list-item.component.ts
+++ src/app/tours/tours-search/tours-list/tours-list-item.component.ts
@@ -26,51 +26,21 @@ export class ToursListItemComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    console.log('arenderer: ' + this.renderer)
-    console.log('atourImage: ' + this.tourImage);
     if (this.tour.updloadedThumbnail) {
-      // let reader = new FileReader();
-      //
-      // // Add an event listener to deal with the file when the reader is complete
-      // reader.addEventListener("load", (event) => {
-      //
-      //   this.tourImage.nativeElement.setAttribute('src', event.target.result);
-      //   // Get the event.target.result from the reader (base64 of the image)
-      //
-      //
-      //   // Resize the image
-      //  // var resized_img = this.resize(img);
-      //
-      //   // Push the img src (base64 string) into our array that we display in our html template
-      //  // this.file_srcs.push(resized_img);
-      // }, false);
-
-      // reader.readAsDataURL(input.files[i]);
 
       let reader = new FileReader();
       let tourImage = this.tourImage;
-      console.log("01tourImage:" + tourImage);
-      console.log('this tourImage: ' + this.tourImage);
+      let renderer = this.renderer;
 
       reader.onload = function (e) {
-        // console.log('this.tour.updloadedThumbnail: ' + this.tour);
-        // console.log('this.tour.updloadedThumbnail: ' + this.tour.updloadedThumbnail);
-        // console.log('e.target.result: ' + e.target.result);
-        console.log("2tourImage:" + tourImage);
-        console.log('this tourImage: ' + this.tourImage);
-        // console.log(reader.result);
-        // console.log('tourImage: ' + this.tourImage);
-         tourImage.nativeElement.setAttribute('src', e.target.result);
-
+        let target:any = e.target;
+        renderer.setElementAttribute(tourImage.nativeElement, 'src', target.result);
       }
       reader.readAsDataURL(this.tour.updloadedThumbnail);
     }
-
   }
 
   onDelete(tour:Tour) {
     this.service.delete(tour);
   }
-
-
 }
